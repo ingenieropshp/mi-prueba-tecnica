6d3e40e8098be23b2ba8e997547ced8a113cc04a
piere steven <ingenieropshp@gmail.com>
2026-04-08 16:07:18 -0500

    fix: simplificar config de open-next para evitar error

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,9 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
+// Configuración de OpenNext para Cloudflare con R2 Cache habilitado
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-	// For best results consider enabling R2 caching
-	// See https://opennext.js.org/cloudflare/caching for more details
-	// incrementalCache: r2IncrementalCache
+    // Se habilita R2 para permitir Incremental Static Regeneration (ISR)
+    // Asegúrate de tener un bucket de R2 vinculado en tu wrangler.jsonc
+    incrementalCache: r2IncrementalCache,
 });
