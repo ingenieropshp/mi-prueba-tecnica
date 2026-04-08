a3ec49f85cc67c1adb75c9615073ceaa440e5acc
piere steven <ingenieropshp@gmail.com>
2026-04-08 13:24:48 -0500

    fix: evitar bucle infinito en el build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
+const config = {
+  default: {
+    buildCommand: "npm run next-build",
+  },
 };
 
-export default nextConfig;
+export default config;
